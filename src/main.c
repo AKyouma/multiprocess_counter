39d585edb93707e05f159f8639909d279ebeb619
AKyouma <augustopta32@gmail.com>
2020-06-16 03:17:50 -0300

    Versão Final

--- src/main.c
+++ src/main.c
@@ -1,8 +1,7 @@
-/* Contador de Primos
- *
- * 
- *
- *
+/* Contador de primos com multiprocessador
+ * Data de  submissao: 16/06/20
+ * Nome: Augusto Lourenço de Faria
+ * RA: 194447
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,7 +16,7 @@ int primo (int numero){
 	int i;
 	if (numero == 1 || numero == 0)
 		return 0;
-	for (i=2; i<=numero/2; i++){
+	for (i=2; i < numero; i++){
 		if (numero % i == 0)
 			return 0;
 	}
@@ -27,7 +26,7 @@ int primo (int numero){
 int main() {
 	pid_t p;
 	int *resultado;
-	int a, filhos=0;
+	int entrada, filhos=0;
 
 	/*Definir flags de protecao e visibilidade de memoria*/
 	int protection = PROT_READ | PROT_WRITE;
@@ -36,26 +35,28 @@ int main() {
 	/*Criação da memória compartilhada para o resultado*/
 	resultado = (int*) mmap(NULL, sizeof(int), protection, visibility, 0, 0);
 	
-
-	
 	do{
 		/*lê a entrada*/
-		scanf("%d", &a);
+		scanf("%d", &entrada);
 		
+		/*verifica se já tem 4 filhos e espera acabar um para poder continuar, retirei
+		  a parte de tirar todos os filhos, pois não havia necessidade já que se tem um
+		  disponível podemos continuar*/
 		if(filhos == 4){
 			while (waitpid(p, NULL, 0) > 0){
 					filhos--; 
 			}
 		}
 		
+		/*cria os filhos*/
 		p = fork();
 		filhos++;
 		if(p == 0){
-			(*resultado) += primo(a);
+			(*resultado) += primo(entrada);
 			exit(0);	
 		}
 	}
-	while((a = getchar()) != '\n');
+	while((entrada = getchar()) != '\n');
 	
 	/*espera todos os filhos acabarem*/
 	while(wait(NULL) > 0); 
