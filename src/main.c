6402e8280a171161b3f14efd49b64f8b32c3817b
AKyouma <augustopta32@gmail.com>
2020-06-16 02:25:46 -0300

    Testes errados, comentado

--- src/main.c
+++ src/main.c
@@ -33,30 +33,36 @@ int main() {
 	/*Definir flags de protecao e visibilidade de memoria*/
 	int protection = PROT_READ | PROT_WRITE;
 	int visibility = MAP_SHARED | MAP_ANON;
-
+	
+	/*Criação da memória compartilhada*/
 	mem_compartilhada = (int*) mmap(NULL, sizeof(int)*500, protection, visibility, 0, 0);
-
+	
+	/*seta aponta para percorrer o vetor da entrada*/
 	aponta = &(mem_compartilhada[0]);
 	*aponta = 1;
 
+	/*lê a entrada*/
 	while((a = getchar()) != '\n'){
 
 		scanf("%d", &(mem_compartilhada[(*aponta)]));
 		(*aponta)++;
 	}
 	
+	/*cria os processos filhos, com máximo de 4*/
 	for(int filho=0; filho < 4; filho++){
 		p[filho] = fork();
 		if(p[filho] == 0)
 			break;
 	}
-
+	
+	/*percorre a memoria compartilhada averiguando cada numero pego na entrada se é primo ou não*/
   	while ((*aponta) >= 1){
 			(*aponta)--;
 			resultado = resultado + primo(mem_compartilhada[(*aponta)]);
 	}
-
-	while(wait(&final) != -1){};
+	
+	/*espera todos os filhos acabarem*/
+	while(wait(&final) != -1);
 
 	printf("%d\n", resultado);
 	
